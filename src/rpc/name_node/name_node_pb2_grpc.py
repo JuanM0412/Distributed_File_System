cdcf6d8e50778afa13122f1d43386dfa027567e1
JuanM0412 <juangomezp1204@gmail.com>
2024-09-12 20:13:10 -0500

    fix: the errors introduced by the change in name_node.proto have been corrected.

--- src/rpc/name_node/name_node_pb2_grpc.py
+++ src/rpc/name_node/name_node_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import name_node_pb2 as name__node__pb2
+from . import name_node_pb2 as name__node__pb2
 
 GRPC_GENERATED_VERSION = '1.66.1'
 GRPC_VERSION = grpc.__version__
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 from src.rpc.data_node import data_node_pb2
 from config import CHUNK_SIZE
+import os
 
 
 def GetFileChunks(file_path):
@@ -15,3 +16,8 @@ def SaveChunksToFile(chunks, filename):
     with open(filename, 'wb') as f:
         for chunk in chunks:
             f.write(chunk.buffer)
+
+
+def GetFileSize(file_path):
+    size = os.path.getsize(file_path)
+    return float(size / CHUNK_SIZE) # CHUNK_SIZE is 1024*1024 bytes = 1MB
